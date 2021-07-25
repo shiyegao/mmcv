13d635d660378f03c9b93b42dce13f7107454f40
Dequan Wang <dequanwang@berkeley.edu>
2021-07-25 05:45:09 -0700

    add SampleNoormOptimizerConstructor

--- mmcv/runner/optimizer/__init__.py
+++ mmcv/runner/optimizer/__init__.py
@@ -1,8 +1,8 @@
 from .builder import (OPTIMIZER_BUILDERS, OPTIMIZERS, build_optimizer,
                       build_optimizer_constructor)
 from .default_constructor import DefaultOptimizerConstructor
-
+from .samplenorm_constructor import SampleNormOptimizerConstructor
 __all__ = [
     'OPTIMIZER_BUILDERS', 'OPTIMIZERS', 'DefaultOptimizerConstructor',
-    'build_optimizer', 'build_optimizer_constructor'
+    'build_optimizer', 'build_optimizer_constructor', 'SampleNormOptimizerConstructor'
 ]
